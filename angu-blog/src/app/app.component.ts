8d92b4bf26b506fc05149fd48c0558bd84b7c722
kimbeff <kimbeff@gmail.com>
2024-12-11 22:48:24 +0200

    logout++
    For sure that way looks much more better. The ++ is for the private routes which are now divided from the public.

--- angu-blog/src/app/app.component.ts
+++ angu-blog/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angu-blog';
+export class AppComponent implements OnInit {
+  isLoggedIn = false;
+
+  constructor(private auth: AuthService) {}
+
+  ngOnInit(): void {
+    this.auth.getUser().subscribe(user => {
+      this.isLoggedIn = !!user; // Check if user is logged in
+    });
+  }
+
+  logout(): void {
+    this.auth.logout().then(() => {
+      this.isLoggedIn = false; // Update the UI
+    });
+  }
 }
